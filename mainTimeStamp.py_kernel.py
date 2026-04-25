59ac0623e0ff32b3139229204048171c37732de4
Rebeca S Carmo <rebeca.carmo@ucsal.edu.br>
2026-04-25 00:41:27 -0300

    atualiza timestamp relativo

---
+++ mainTimeStamp.py_kernel.py
@@ -0,0 +1,68 @@
+from machine import I2C, Pin
+import ahtx0
+import time
+from hardware import oled
+import machine
+import utime
+import select
+import sys
+
+
+i2c = I2C(0, scl=Pin(1), sda=Pin(0))
+
+
+sensor = ahtx0.AHT20(i2c)
+
+ultimoTempo = 0
+momentoRegistro = 0
+poller = select.poll()
+poller.register(sys.stdin, select.POLLIN)
+
+inicio = utime.ticks_ms()
+
+while True:
+    if poller.poll(0):
+        if(sys.stdin.readline().strip() == "dump"):
+            try:
+                with open('dadosCSV.csv','r') as f:
+                    for i in f.readlines():
+                        print(i)
+            except:
+                print("Nenhum dado registrado ainda")
+    if(utime.ticks_diff(utime.ticks_ms(), ultimoTempo) > 60000):
+        ultimoTempo = utime.ticks_ms()
+        
+        temp = sensor.temperature
+        hum = sensor.relative_humidity
+        momentoRegistro = "{}:{}".format(
+            (int(utime.ticks_diff(utime.ticks_ms(), inicio)/1000)//60),
+            (int(utime.ticks_diff(utime.ticks_ms(), inicio)/1000)%60)
+        )
+        
+        temp_str = "{:.2f} C".format(temp)
+        hum_str = "{:.2f} %".format(hum)
+
+     
+        oled.fill(0)
+
+        
+        oled.text("Temp:" + temp_str, 0, 0) 
+
+        oled.text("Umid:" + hum_str, 0, 20)
+        
+        oled.text("Moment:" + momentoRegistro, 0, 40)
+        
+
+        oled.show()
+
+        
+        print("Temp:", temp_str)
+        print("Umid:", hum_str)
+        print("Momento:", momentoRegistro)
+        print("------------------")
+        leitura = (temp, hum, momentoRegistro)
+        
+        writeFormat = "{}, {}, {}\n".format(leitura[0],leitura[1], leitura[2])
+        with open('dadosCSV.csv','a') as f:
+            f.write(writeFormat)
+          
